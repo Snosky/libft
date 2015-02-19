1c389d862c4b55c897d55fa99160fd98a972b53b
Tristan Payen <s.nos.ky@hotmail.fr>
2015-02-19 19:09:03 +0100

    Erreur de casting malloc

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: tpayen <tpayen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/15 18:16:44 by tpayen            #+#    #+#             */
-/*   Updated: 2015/01/21 19:32:37 by tpayen           ###   ########.fr       */
+/*   Updated: 2015/02/19 19:08:44 by tpayen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 	}
 	else
 	{
-		if (!(ret->content = (t_list *)malloc(sizeof(content_size + 1))))
+		if (!(ret->content = (void *)malloc(sizeof(content_size + 1))))
 			return (NULL);
 		ret->content = ft_memcpy(ret->content, content, content_size);
 	}
