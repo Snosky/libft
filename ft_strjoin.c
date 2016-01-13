1ac0e7e610d5540ae84a44e28d5bec13e5997cf6
Tristan PAYEN <tpayen@student.42.fr>
2016-01-13 18:56:58 +0100

    Correction bug

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: tpayen <tpayen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/15 17:00:56 by tpayen            #+#    #+#             */
-/*   Updated: 2015/01/15 17:03:29 by tpayen           ###   ########.fr       */
+/*   Updated: 2016/01/13 18:55:30 by tpayen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,12 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*ret;
 
+	if (*s1 && !(*s2))
+		return (ft_strdup(s1));
+	if (!(*s1) && *s2)
+		return (ft_strdup(s2));
+	if (!(*s1))
+		return (NULL);
 	if (!(ret = ft_strnew(ft_strlen(s1) + ft_strlen(s2) + 1)))
 		return (NULL);
 	ret = ft_strcat(ret, s1);
