4abef68e6eacf9ad6353ef5354b5c891d62e5a2c
Tristan PAYEN <tpayen@e2r5p17.42.fr>
2016-02-27 15:13:23 +0100

    Better ft_strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: tpayen <tpayen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/15 17:00:56 by tpayen            #+#    #+#             */
-/*   Updated: 2016/01/13 18:55:30 by tpayen           ###   ########.fr       */
+/*   Updated: 2016/02/27 15:10:50 by tpayen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*ret;
 
-	if (*s1 && !(*s2))
+	if (s1 != NULL && s2 == NULL)
 		return (ft_strdup(s1));
-	if (!(*s1) && *s2)
+	if (s1 == NULL && s2 != NULL)
 		return (ft_strdup(s2));
-	if (!(*s1))
+	if (s1 == NULL)
 		return (NULL);
 	if (!(ret = ft_strnew(ft_strlen(s1) + ft_strlen(s2) + 1)))
 		return (NULL);
