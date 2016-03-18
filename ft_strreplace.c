b2f3d10476a7bb7aac5dfc1368226206e252d362
tpayen <tpayen@student.42.fr>
2016-03-18 18:32:28 +0100

    strreplace no more work on param string

--- ft_strreplace.c
+++ ft_strreplace.c
@@ -6,7 +6,7 @@
 /*   By: tpayen <tpayen@studio.42.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/12 17:14:05 by tpayen            #+#    #+#             */
-/*   Updated: 2016/03/15 19:20:44 by tpayen           ###   ########.fr       */
+/*   Updated: 2016/03/18 18:31:51 by tpayen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,14 @@ char	*ft_strreplace(char *search, char *replace, char *s)
 {
 	char	*ret;
 	char	*str;
+	char	*tmp;
 
+	tmp = ft_strdup(s);
 	ret = NULL;
-	if ((str = ft_strstr(s, search)) == NULL)
+	if ((str = ft_strstr(tmp, search)) == NULL)
 		return (NULL);
 	str[0] = 0;
-	ret = ft_strdup(s);
+	ret = ft_strdup(tmp);
 	ret = ft_strjoin(ret, replace);
 	ret = ft_strjoin(ret, str + ft_strlen(search));
 	if (ft_strstr(ret, search))
