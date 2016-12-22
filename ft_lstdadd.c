fe5c5dab438241592c81ab9d589d4584899a5d35
Snosky <tpayen@student.42.fr>
2016-12-22 14:35:54 +0100

    Update ft_lstdadd.c

--- ft_lstdadd.c
+++ ft_lstdadd.c
@@ -6,25 +6,33 @@
 /*   By: tpayen <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/25 16:51:16 by tpayen            #+#    #+#             */
-/*   Updated: 2016/11/29 18:55:40 by tpayen           ###   ########.fr       */
+/*   Updated: 2016/12/22 14:35:30 by tpayen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
-void	ft_lstdadd(t_lstd **alst, t_lstd *new)
+void	ft_lstdadd(t_lstd **alst, t_lstd *new, int prev)
 {
 	if (!*alst)
-	{
-		new->next = new;
-		new->prev = new;
 		*alst = new;
-	}
 	else
 	{
-		new->prev = (*alst)->prev;
-		new->next = *alst;
-		(*alst)->prev->next = new;
-		(*alst)->prev = new;
+		if (prev)
+		{
+			new->next = *alst;
+			new->prev = (*alst)->prev;
+			if ((*alst)->prev)
+				(*alst)->prev->next = new;
+			(*alst)->prev = new;
+		}
+		else
+		{
+			new->next = (*alst)->next;
+			new->prev = *alst;
+			if ((*alst)->next)
+				(*alst)->next->next = new;
+			(*alst)->next = new;
+		}
 	}
 }
